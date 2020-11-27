8c6048541b50daa4121969cc162c0fc3bf182233
Mark Rogaski <mrogaski@pobox.com>
2020-11-27 17:15:07 -0500

    Added DIET validation.

--- diet.go
+++ diet.go
@@ -2,8 +2,9 @@ package diet
 
 import "github.com/go-playground/validator/v10"
 
+// DiscreteIntervalEncodingTree is a structure for storing subsets of integers as encoded intervals.
 type DiscreteIntervalEncodingTree struct {
-	Interval *Interval                     // The interval contained by the node
+	Interval *Interval                     `validate:"required_with=Left Right"` // The interval contained by the node
 	Left     *DiscreteIntervalEncodingTree // Intervals preceding the node interval
 	Right    *DiscreteIntervalEncodingTree // Intervals succeeding the node interval
 }
@@ -11,6 +12,25 @@ type DiscreteIntervalEncodingTree struct {
 // IsEmpty tests whether the tree contains any intervals.
 func (diet *DiscreteIntervalEncodingTree) IsEmpty() bool {
 	return diet.Interval == nil
+}
+
+// Validate checks the validity of the DiscreteIntervalEncodingTree struct and returns errors if the struct is invalid.
+func (diet *DiscreteIntervalEncodingTree) Validate() error {
+	if diet == nil {
+		return nil
+	}
+
+	err := diet.Left.Validate()
+	if err != nil {
+		return err
+	}
+
+	err = diet.Right.Validate()
+	if err != nil {
+		return err
+	}
+
+	return validate.Struct(diet)
 }
 
 // single instance of Validate to cache struct information
--- diet_test.go
+++ diet_test.go
@@ -14,3 +14,34 @@ func TestDiscreteIntervalEncodingTreeIsEmpty(t *testing.T) {
 	tree.Interval = &Interval{0, 2}
 	assert.False(t, tree.IsEmpty())
 }
+
+// Tree validity
+func TestDiscreteIntervalEncodingTreeValidateEmpty(t *testing.T) {
+	tree := DiscreteIntervalEncodingTree{}
+	assert.NoError(t, tree.Validate())
+}
+
+func TestDiscreteIntervalEncodingTreeValidate(t *testing.T) {
+	left := &DiscreteIntervalEncodingTree{Interval: &Interval{0, 2}}
+	right := &DiscreteIntervalEncodingTree{Interval: &Interval{8, 10}}
+	tree := &DiscreteIntervalEncodingTree{Interval: &Interval{4, 6}, Left: left, Right: right}
+	assert.NoError(t, tree.Validate())
+}
+
+func TestDiscreteIntervalEncodingTreeValidateInvalid(t *testing.T) {
+	sub := &DiscreteIntervalEncodingTree{Interval: &Interval{0, 2}}
+	tree := &DiscreteIntervalEncodingTree{Left: sub}
+	assert.Error(t, tree.Validate())
+
+	tree = &DiscreteIntervalEncodingTree{Right: sub}
+	assert.Error(t, tree.Validate())
+}
+
+func TestDiscreteIntervalEncodingTreeValidateInvalidSubtree(t *testing.T) {
+	sub := &DiscreteIntervalEncodingTree{Left: &DiscreteIntervalEncodingTree{}}
+	tree := &DiscreteIntervalEncodingTree{Interval: &Interval{0, 2}, Left: sub}
+	assert.Error(t, tree.Validate())
+
+	tree = &DiscreteIntervalEncodingTree{Interval: &Interval{0, 2}, Right: sub}
+	assert.Error(t, tree.Validate())
+}
